269a0ecaafc4427c409f67851008712686735fa7
Jared Taylor <jared.xyz@protonmail.com>
2019-03-17 12:13:58 -0400

    Reorganize

---
+++ src/jash_core_commands.cc
@@ -0,0 +1,49 @@
+//
+// Created by jared on 10/3/18.
+//
+
+#include "jash_core_commands.h"
+
+int jash_cd(std::vector<std::string> args) {
+    if (args[1].empty()) {
+        std::cerr << "\njash: expected argument to \"cd\"" << std::endl;
+    }
+    else {
+        if (chdir(args[1].c_str()) not_eq 0) perror("jash");
+    }
+    return 1;
+}
+
+int jash_help(std::vector<std::string> args) {
+    std::cout << "\njash - Just Another Shell\n" << std::endl;
+    std::cout << "Core commands include: " << std::endl;
+
+    for (std::string i : core_list) {
+        std::cout << "- " << i << std::endl;
+    }
+
+    std::cout << "\nwritten by Jared Taylor" << std::endl;
+    std::cout << "see more: https://github.com/Jragoon/jash-shell\n" << std::endl;
+    return 1;
+}
+
+int jash_exit(std::vector<std::string> args) {
+    return 0;
+}
+
+int jash_clear(std::vector<std::string> args) {
+    // \033[2J clear the screen, \0333 positions the cursor at (row 1, column 1)
+    std::cout << "\033[2J\033[1;1H";
+    return 1;
+}
+
+int jash_handler(int function_code, std::vector<std::string> args) {
+    switch(function_code) {
+        case 0: return jash_cd(args);
+        case 1: return jash_help(args);
+        case 2: return jash_exit(args);
+        case 3: return jash_clear(args);
+        // Something went wrong
+        default: return 0;
+    }
+}
---
+++ src/main.cc
@@ -0,0 +1,10 @@
+//
+// Created by jared on 10/3/18.
+//
+
+#include "jash_loop.h"
+
+int main(int argc, char **argv) {
+    jash_loop();
+    return EXIT_SUCCESS;
+}
